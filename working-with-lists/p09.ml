dce56bf5f70c0f65f6082f45824caefdba7a2d21
Zhe Wang <zhexwang@gmail.com>
2013-05-22 16:23:24 +0800

    1st version of the solution ot p09

---
+++ working-with-lists/p09.ml
@@ -0,0 +1,30 @@
+(* Problem:
+   Pack consecutive duplicates of list elements into sublists.
+
+   Function Type:
+   val pack : 'a list -> ('a list) list
+
+   E.g.,
+   pack [`a;`a;`a;`a;`b;`c;`c;`a;`a;`d;`d;`e;`e;`e;`e]
+   = [[`a;`a;`a;`a]; [`b]; [`c;`c]; [`a;`a]; [`d;`d]; [`e;`e;`e;`e]];;
+ *)
+
+let pack (lst : 'a list) : ('a list) list =
+  let rec loop (acc : ('a list) list) (sub_acc : 'a list) (l : 'a list) : 
+  ('a list) list =
+    match l with
+    | a :: (b :: t as tl) ->
+      let sub_acc = a :: sub_acc
+      in
+      (*Pattern for general recursion*)
+      if t != [] then
+        if a = b then loop acc sub_acc tl
+        else loop (sub_acc :: acc) [] tl
+      (* Pattern for the last two elements *)
+      else 
+        if a = b then (b :: sub_acc) :: acc
+        else [b] :: (sub_acc :: acc)
+    (* Patterns for [] (empty list) and _ :: [] (list with only one element) *) 
+    | others -> sub_acc :: acc
+  in List.rev ( loop [] [] lst)
+;;
