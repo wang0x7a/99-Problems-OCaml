321085feeb6923e1ea7bc9826389b15f90cd9eaf
Zhe Wang <zhexwang@gmail.com>
2013-05-22 13:53:07 +0800

    revised solution to p08

--- working-with-lists/p08.ml
+++ working-with-lists/p08.ml
@@ -5,6 +5,7 @@
    val compress : 'a list -> 'a list
  *)
 
+(* 1. Tail-recursive version *)
 let compress (lst : 'a list) : 'a list =
   let rec loop (acc : 'a list) (l : 'a list) : 'a list =
     match l with
@@ -18,3 +19,30 @@ let compress (lst : 'a list) : 'a list =
         else loop ( h :: acc) t
   in List.rev (loop [] lst)
 ;;
+
+(* 2. Non-tail-recursive version by VictorNicollet, but more elegant. *)
+let rec compress (lst : 'a list) : 'a list =
+  match lst with
+  | a :: (b :: _ as t) ->
+    (* The difference between the Implementaion 1 and 2 is that, 1 checks the
+       duplicates upon its first occurrence, while 2 checks only at the 
+       boundary between two different elements.
+     *) 
+    if a = b then compress t
+    else a :: compress t
+  (* match all other cases *)
+  | others -> others
+;;
+
+(* 3. Tail-recursive version of 2. *)
+let compress (lst : 'a list) : 'a list =
+  let rec loop (acc : 'a list) (l : 'a list) : 'a list =
+    match l with
+    | a :: (b :: _ as t) ->
+      if a = b then loop acc t
+      else loop (a :: acc) t
+    (* cases for [] and [`a] *)
+    | others -> others @ acc
+  in List.rev (loop [] lst)
+;;
+
