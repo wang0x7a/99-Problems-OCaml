b8b22ab5e8999fa991d19f88b8aba5e88a514d3b
Zhe Wang <zhexwang@gmail.com>
2013-05-25 07:08:53 +0800

    first version of the solution to p17

---
+++ working-with-lists/p17.ml
@@ -0,0 +1,19 @@
+(* Problem:
+   Split a list into two parts; the length of the first part is given.
+   If the length of the first part is longer than the entire list, then the 
+   first part is the list and the second part is empty.
+
+   Function Type:
+   val split : 'a list -> int -> ('a list) * ('a list)
+ *)
+
+let split (lst : 'a list) (n : int) : ('a list) * ('a list) =
+  if n > List.length lst then (lst, [])
+  else
+    let rec loop (tmp : 'a list) (i : int) = function
+    | [] -> ([], [])
+    | h :: t ->
+      if i = 1 then (tmp @ [h], t)
+      else loop (tmp @ [h]) (i - 1) t
+    in loop [] n lst
+;;
