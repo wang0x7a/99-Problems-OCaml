2aadaf07ebd61cfb98db232cda7e5e8b2dfcf6e2
Zhe Wang <zhexwang@gmail.com>
2013-05-24 16:32:21 +0800

    modify the solution to p16

--- working-with-lists/p16.ml
+++ working-with-lists/p16.ml
@@ -9,11 +9,11 @@
    skip the specific ones.
  *)
 let drop (lst : 'a list) (step : int) : 'a list =
-  let rec loop (acc : 'a list) (l : 'a list) (i : int) (n : int): 'a list =
+  let rec loop (acc : 'a list) (l : 'a list) (i : int) : 'a list =
     match l with
     | [] -> acc
     | h :: t -> 
-      if i = 1 then loop acc t n n
-      else loop (h :: acc) t (i - 1) n
-  in List.rev (loop [] lst step step)
+      if i = 1 then loop acc t step
+      else loop (h :: acc) t (i - 1)
+  in List.rev (loop [] lst step)
 ;;
