8a5392b305007bc1af61ee46a322d454162a6781
Zhe Wang <zhexwang@gmail.com>
2013-05-27 13:09:30 +0800

    first solution to p21

---
+++ working-with-lists/p21.ml
@@ -0,0 +1,16 @@
+(* Problem:
+   Insert an element at a given position into a list.
+   Start counting list elements with 0.
+
+   Function Type:
+   val insert_at : int -> 'a -> 'a list
+ *)
+
+let insert_at (elem : 'a) (k : int) (lst : 'a list) : 'a list =
+  let rec search (acc : 'a list) (i : int) = function
+  | [] -> acc @ [elem]
+  | h :: t as l->
+    if i = k then (acc @ [elem]) @ l
+    else search (acc @ [h]) (i + 1) t
+  in search [] 0 lst
+;;
