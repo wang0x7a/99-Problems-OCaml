f8c8443e2ab5668303687f1b0163544fe76d124f
Zhe Wang <zhexwang@gmail.com>
2013-05-27 14:58:51 +0800

    2nd solution to p22

--- working-with-lists/p22.ml
+++ working-with-lists/p22.ml
@@ -11,10 +11,21 @@ let range (p : int) (q : int) : int list =
   let s = min p q
   and e = max p q in
   
-  let rec aux (acc : 'a list) (i : int) : 'a list =
+  let rec aux (acc : int list) (i : int) : int list =
     if i <= e then aux (i :: acc) (i + 1)
     else
       if p = s then List.rev acc
       else acc
   in aux [] s
+;;
+
+(* Implemenation by VictorNicollet *)
+let range (p : int) (q : int) : int list =
+  let rec aux (acc : int list) (a : int) (b : int) : int list =
+    if a > b then []
+    else if a = b then a :: acc
+    else aux (a :: acc) (a + 1) b
+  in
+    if p <= q then List.rev (aux [] p q)
+    else aux [] q p
 ;;
