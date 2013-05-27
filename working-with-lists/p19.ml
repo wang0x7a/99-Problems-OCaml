8c7a0deed3d7e5a68dc5c3b80fb66cef2a344b1a
Zhe Wang <zhexwang@gmail.com>
2013-05-27 10:59:20 +0800

    first version of the solution to p19

---
+++ working-with-lists/p19.ml
@@ -0,0 +1,20 @@
+(* Problem:
+   Rotate a list N places to the left, and when the steps are negative,
+   rotate to the right.
+
+   Function Type:
+   val rotate : 'a list -> int -> 'a list
+ *)
+
+let rotate (lst : 'a list) (n : int) : 'a list =
+  let len = List.length lst in
+  if len = 0 then []
+  else 
+    let n = (len + n) mod len in
+    let rec loop (acc : 'a list) (i : int) = function
+    | [] -> []
+    | h :: t as l-> 
+      if i = 0 then l @ (List.rev acc)
+      else loop (h :: acc) (i - 1) t
+    in loop [] n lst
+;;
