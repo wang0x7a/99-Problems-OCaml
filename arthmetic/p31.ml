eccdee8f74fecc6e74175bd92c18f84dce7d9071
Zhe Wang <zhexwang@gmail.com>
2013-06-28 17:26:25 +0800

    second solution to p31

--- arthmetic/p31.ml
+++ arthmetic/p31.ml
@@ -6,3 +6,13 @@ let is_prime (num : int) : bool =
     else if num mod divisor = 0 then false
     else loop (divisor - 1)
   in loop (num / 2)
+;;
+
+(* Instead of considering each possible divisor, we can also take a pair
+ * of divisors into consideration directly, reducing the time complexity 
+ * from n/2 down to sqrt(n) *)
+let is_prime (num : int) : bool =
+  let rec loop (d : int) : bool =
+    (d * d > num) || (num mod d <> 0 && loop (d + 1))
+  in loop 2
+;;
