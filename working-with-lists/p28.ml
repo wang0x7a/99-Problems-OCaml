ae787e38adefd785082fa8bf9d3f30ca4a9f43bb
Zhe Wang <zhexwang@gmail.com>
2013-06-28 16:44:20 +0800

    pend solving p28

---
+++ working-with-lists/p28.ml
@@ -0,0 +1,14 @@
+(* Problem:
+ * Sorting a list of lists according to length of sublists
+ * 
+ * a) We suppose that a list contains elements that are lists themselves. 
+ *    The objective is to sort the elements of this list according to their
+ *    length. E.g., short lists first, longer lists later, or vice versa.
+ *
+ * b) Again, we suppose that a list contains elements that are lists 
+ *    themselves. But this time the objective is to sort the elements of this 
+ *    list according to their length frequency; i.e., in the default, where 
+ *    sorting is done ascendingly, lists with rare length are placedfirst, 
+ *    others with a more frequent length come later.
+ *)
+
