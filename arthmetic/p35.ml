2447c8cdd2b15addcca3212e86a6a9756326f6ec
Zhe Wang <zhewang@redhat.com>
2013-07-04 11:37:50 +0800

    polish the solution to p35

--- arthmetic/p35.ml
+++ arthmetic/p35.ml
@@ -23,3 +23,23 @@ let factors (n : int) : int list =
     if n = 1 then [] else [n]
   | l -> l
 ;;
+
+(* List all prime divisors including duplicates *)
+let factors (n : int) : int list =
+  let rec aux (acc : int list) (d : int) (c : int) : int list =
+    if d = 1 then acc
+    else
+      (* if is_prime d && c mod d = 0 
+       * there is no need to test whether d is a prime each time,
+       * unless c < d, d will be a prime *)
+      if c < n && c mod d = 0 then aux (d :: acc) d (c / d)
+      else
+        if n mod (d - 1) = 0 && is_prime (d - 1)
+        then aux ((d - 1) :: acc) (d - 1) (n / (d - 1))
+        else aux acc (d - 1) n
+  in
+  match (aux [] (n / 2 + 1) n) with
+  | [] ->
+    if n = 1 then [] else [n]
+  | l -> l
+;;
