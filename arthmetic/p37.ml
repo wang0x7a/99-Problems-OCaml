be92a0079a510e6eac65d3768912121ec6bdfc55
Zhe Wang <zhexwang@gmail.com>
2013-07-04 21:24:35 +0800

    solution to p37

--- arthmetic/p37.ml
+++ arthmetic/p37.ml
@@ -12,3 +12,31 @@
  * phi(m) = (p1 - 1) * p1 ^ (m1 - 1) * (p2 - 1) * p2 ^ (m2 - 1) * ...
  *)
 
+let factors (n : int) : (int * int) list =
+  let rec aux (acc : (int * int) list) (d : int) (count : int) (i : int)
+  : (int * int) list =
+    if i = 1 then
+      if count = 0 then acc
+      else (d, count) :: acc
+    else
+      if i mod d = 0 then aux acc d (count + 1) (i / d)
+      else aux (if count = 0 then acc else (d, count) :: acc) (d + 1) 0 i
+  in
+  aux [] 2 0 n
+
+(* Calculate x^y *)
+let pow (x : int) (y : int) : int =
+  let rec aux (acc : int) (count : int) =
+    if count = 0 then acc
+    else aux (acc * x) (count - 1)
+  in
+  aux 1 y
+
+let phi (m : int) : int =
+  let fac = factors m in
+  let rec aux (acc : int) = function
+  | [] -> acc
+  | (p, m) :: t -> aux ((p - 1) * (pow p (m - 1)) * acc) t
+  in
+  aux 1 fac
+;;
