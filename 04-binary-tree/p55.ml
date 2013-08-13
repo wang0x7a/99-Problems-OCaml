6c7c6d482a1fd3e18c5538fde580cdb9cd5c5552
Zhe Wang <zhewang@redhat.com>
2013-08-13 16:59:12 +0800

    problem description p55

---
+++ 04-binary-tree/p55.ml
@@ -0,0 +1,14 @@
+(* Problem 55:
+ * Construct completely balanced binary trees
+ *
+ * In a completely balanced binary tree, the following property holds for
+ * every node: The number of nodes in its left subtree and the number of
+ * nodes in its right subtree are almost equal, which means their
+ * difference is not greater than one.
+ *
+ * Write a function #cbal_tree# to construct completely balanced bianry
+ * trees for a given number of nodes. The function should generate all 
+ * solutions iva backtracing. Put 'x' as information into all nodes of
+ * the tree.
+ *)
+
