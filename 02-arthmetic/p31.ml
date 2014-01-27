22bcae80cfeb3bc81b81de6288a3c27d0800681c
zw267 <zhexwang@gmail.com>
2014-01-28 00:39:55 +0800

    revisit p31

--- 02-arthmetic/p31.ml
+++ 02-arthmetic/p31.ml
@@ -20,3 +20,15 @@ let is_prime (num : int) : bool =
       (d * d > num) || (num mod d <> 0 && loop (d + 1))
     in num <> 1 && loop 2
 ;;
+
+(* Revisit *)
+let is_prime n =
+  let rec loop i =
+    if i * i <= n then
+      if n mod i <> 0 then loop (i + 1)
+      else false
+    else
+      true
+  in
+  n <> 1 && loop 2
+;;
