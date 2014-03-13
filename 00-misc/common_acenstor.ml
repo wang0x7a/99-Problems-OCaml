4c12b125b9c02581cd9a7868364007ccbd0e97be
zw267 <zhexwang@gmail.com>
2014-03-13 20:12:28 +0800

    make the output clean

--- 00-misc/common_acenstor.ml
+++ 00-misc/common_acenstor.ml
@@ -2,10 +2,13 @@
 
 open Core.Std
 
+exception Not_found
+
 type 'a binary_tree = 
   | Empty
   | Node of 'a * 'a binary_tree * 'a binary_tree
 
+(*
 let rec lca root p q =
   match (root, p, q) with
   | (Empty, _, _) | (_, Empty, _) | (_, _, Empty) -> Empty
@@ -19,4 +22,26 @@ let rec lca root p q =
       | (Node (_, _, _), Empty) -> left
       | (Empty, Node (_, _, _)) -> right
       | (Empty, Empty) -> Empty
+;;
+*)
+
+(* Only extrac the value of the ancestor *)
+let lca root p q =
+  let rec helper root p q =
+    match (root, p, q) with
+    | (Empty, _, _) | (_, Empty, _) | (_, _, Empty) -> Empty
+    | (Node (x, left, right), Node (y, _, _), Node (z, _, _)) ->
+        if x = y || x = z then root
+        else
+          let left = helper left p q in
+          let right = helper right p q in
+          match (left, right) with
+          | (Node (_, _, _), Node (_, _, _)) -> root
+          | (Node (_, _, _), Empty) -> left
+          | (Empty, Node (_, _, _)) -> right
+          | (Empty, Empty) -> Empty 
+  in
+  match (helper root p q) with
+  | Emtpy -> raise Not_found
+  | Node (x, _, _) -> x
 ;;
