f7c7f0e32224ff56d69376002b9a2ed782f1a71f
zw267 <zhexwang@gmail.com>
2014-03-13 19:07:22 +0800

    reformat the code

--- 00-misc/common_acenstor.ml
+++ 00-misc/common_acenstor.ml
@@ -7,17 +7,16 @@ type 'a binary_tree =
   | Node of 'a * 'a binary_tree * 'a binary_tree
 
 let rec lca root p q =
-         match (root, p, q) with
-         | (Empty, _, _) | (_, Empty, _) | (_, _, Empty) -> Empty
-         | (Node (x, left, right), Node (y, _, _), Node (z, _, _)) ->
-           if x = y || x = z then root
-           else
-             let left = lca left p q in
-             let right = lca right p q in
-             match left, right with
-             | (Node (_, _, _), Node (_, _, _)) -> root
-             | (Node (_, _, _), Empty) -> left
-             | (Empty, Node (_, _, _)) -> right
-             | (Empty, Empty) -> Empty
-        ;;
-
+  match (root, p, q) with
+  | (Empty, _, _) | (_, Empty, _) | (_, _, Empty) -> Empty
+  | (Node (x, left, right), Node (y, _, _), Node (z, _, _)) ->
+    if x = y || x = z then root
+    else
+      let left = lca left p q in
+      let right = lca right p q in
+      match left, right with
+      | (Node (_, _, _), Node (_, _, _)) -> root
+      | (Node (_, _, _), Empty) -> left
+      | (Empty, Node (_, _, _)) -> right
+      | (Empty, Empty) -> Empty
+;;
